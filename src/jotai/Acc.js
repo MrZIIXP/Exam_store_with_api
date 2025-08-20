4ef3b30cad5e1278bad8348a4167b4af4e4ece6e
MrZIIXP <ziyodulloakhrorov@gmail.com>
2025-08-20 17:54:40 +0500

    Finishing

--- src/jotai/Acc.js
+++ src/jotai/Acc.js
@@ -1,3 +1,4 @@
 import { atom } from 'jotai'
 export const Account = atom(localStorage.getItem('account') || null)
+export const Bag = atom(JSON.parse(localStorage.getItem('bag')) || [])
 export const Favorite = atom(JSON.parse(localStorage.getItem('favorite')) || [])
